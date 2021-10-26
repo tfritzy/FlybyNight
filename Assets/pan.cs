6443f993b68d40bb54f3930d98cb5261aeacb7ee
Tyler Fritz <tfritzy@gmail.com>
2021-10-25 18:00:22 -0700

    Initial commit

---
+++ Assets/pan.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pan : MonoBehaviour
+{
+    // private Rigidbody2D rb;
+    decimal posX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        Time.fixedDeltaTime = 1 / ((float)Application.targetFrameRate);
+    }
+
+    // private bool hasSet = false;
+    void FixedUpdate()
+    {
+        this.transform.Translate(Vector3.right * 10 * Time.deltaTime);
+        // this.transform.position += new Vector3((float)(posX + 10) * Time.deltaTime, 0, 0);
+    }
+}
