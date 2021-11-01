f5d49f509f956a25b23908120c3567bf042704b6
Tyler Fritz <tfritzy@gmail.com>
2021-10-31 20:21:31 -0700

    Adding song manager

---
+++ Assets/Scripts/SongManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class SongManager : MonoBehaviour
+{
+    public AudioClip[] Songs;
+
+    private AudioSource audioSource;
+    private const float TIME_BETWEEN_SONGS = 15f;
+    private int songIndex;
+    private float nextSongTime;
+
+    void Start()
+    {
+        audioSource = this.GetComponent<AudioSource>();
+        songIndex = Random.Range(0, Songs.Length);
+        NextSong();
+    }
+
+    void Update()
+    {
+        if (Time.time > nextSongTime)
+        {
+            NextSong();
+        }
+    }
+
+    private void NextSong()
+    {
+        songIndex += 1;
+        songIndex = songIndex % Songs.Length;
+        audioSource.clip = Songs[songIndex];
+        audioSource.Play();
+        nextSongTime = Time.time + Songs[songIndex].length + TIME_BETWEEN_SONGS;
+    }
+}
